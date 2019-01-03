dae37173c7c2949c53c9f81bae228c8f70525509
cajmeredith <cameronmeredith90@gmail.com>
2019-01-03 12:15:46 +0000

    Init

---
+++ src/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/auto',
+      name: 'auto',
+
+      component: () => import('./views/Auto.vue')
+    },
+  
+    // quick find feature would be fairly simple. Take a variable from the html and find that board. Drop down box is another option.
+  ]
+})
---
+++ src/views/Auto.vue
@@ -0,0 +1,47 @@
+<template>
+    
+    <div class="main">
+        <h1>
+            4chan Auto Board
+        </h1>
+
+             <ul class="list-group list-group-flush" stripe >
+                 <li class="list-group-item" v-for="thread in threads" :key="thread.posts[0].sub">{{
+                     thread.posts[0].sub
+                     }} <br> {{thread.posts[0].com}}</li>
+            </ul>  
+
+    </div>
+
+</template>
+
+<style>
+.posts {
+  text-align: left;
+}
+</style>
+
+
+<script>
+const axios = require("axios");
+
+export default {
+  name: "Auto",
+  data() {
+    return {
+      threads: []
+    };
+  },
+  created() {
+    axios
+      .get("http://a.4cdn.org/o/1.json")
+      .then(response => {
+        this.threads = response.data.threads;
+        console.log(this.threads[0].posts[0].sub);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
